2fcf413093cbdd7123b9336e0eb9d1ef4d8a1e28
ajafri2001 <abdullahjafri2001@gmail.com>
2025-01-04 17:40:24 +0530

    json in scala is surprisingly horrible

--- src/main/scala/lsp/Main.scala
+++ src/main/scala/lsp/Main.scala
@@ -1,6 +1,15 @@
 package lsp
 
 object Main {
-  def main(args : Array[String]) : Unit = 
-    println("Hello World")
+    def main(args: Array[String]): Unit =
+        println("Hello World")
+        println(encodeMessage())
+
+    def encodeMessage(): String =
+        val testJson = """
+                       {
+                         "hello" : 1
+                       }
+                       """
+        testJson
 }
