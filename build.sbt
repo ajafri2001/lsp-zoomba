aa1aae0aa24dc5abac506d7622fd8aab890d4aab
ajafri2001 <abdullahjafri2001@gmail.com>
2025-01-04 18:42:28 +0530

    starting the encoder part of the rpc

--- build.sbt
+++ build.sbt
@@ -5,3 +5,11 @@ name := "lsp-zoomba"
 
 // name of the fat jar
 assembly / assemblyJarName := "lsp-zoomba.jar"
+
+val circeVersion = "0.14.10"
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
--- src/main/scala/lsp/Main.scala
+++ src/main/scala/lsp/Main.scala
@@ -1,15 +1,23 @@
 package lsp
 
+import io.circe._, io.circe.parser._
+
 object Main {
     def main(args: Array[String]): Unit =
         println("Hello World")
         println(encodeMessage())
 
-    def encodeMessage(): String =
-        val testJson = """
-                       {
-                         "hello" : 1
-                       }
-                       """
-        testJson
+    def encodeMessage(): Json =
+        val testJson: String =
+            """
+            {
+              "hello" : 123
+            }
+            """.stripMargin
+        val result: Either[ParsingFailure, Json] = parse(testJson)
+
+        result match
+            case Right(json) => json
+            case Left(value) =>
+                throw new IllegalArgumentException("You are doomed")
 }
