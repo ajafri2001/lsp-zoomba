b3ad3526b55e39f421175a2a8c680e83401c0dcd
ajafri2001 <abdullahjafri2001@gmail.com>
2025-01-05 17:18:59 +0530

    regex matched

--- src/main/scala/lsp/Main.scala
+++ src/main/scala/lsp/Main.scala
@@ -17,6 +17,14 @@ case class RequestMessage(
 // Testing to see if local vim client can send errors and panic
 object Main {
     def main(args: Array[String]): Unit =
-        val input = Source.stdin.getLines().mkString("\n")
-        Logger.write(input)
+        // val input = Source.stdin.getLines().mkString("\n")
+        // Logger.write(input)
+
+        val regexMatch = """Content-Length:(\d+)\r\n""".r
+
+        // random json string to test regex
+
+        val input =
+            "Content-Length:123\r\n\r\n{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"textDocument/completion\",\"params\":{\"key\":\"value\"}}"
+        print(regexMatch.findFirstIn(input))
 }
